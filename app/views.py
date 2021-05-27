3d2c06beb5ffcf94a0f6d6564108d22ef3919061
Abdullah Alnuaimi <abdullahalnuaimi36@gmail.com>
2021-05-27 21:06:09 +0300

    add latest posts section in index page

--- app/views.py
+++ app/views.py
@@ -1,5 +1,12 @@
 from django.views.generic import TemplateView
 
+from .models import Post
+
 
 class IndexView(TemplateView):
     template_name = "app/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["posts"] = Post.objects.filter(status="published")[:3]
+        return context
